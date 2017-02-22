e4bb7d6860b4a09761127257e8c118d0558deae0
Aubacca <peter.rohner@trivadis.com>
2017-02-22 14:32:27 +0100

    Modified server script. Used other icons for the events.

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ var connect = require('connect');
 var serveStatic = require('serve-static');
 
 connect().use(serveStatic('src')).listen(8080, function(){
-    console.log('This homepage is running at http://localhost:8080/index.html ...');
+    console.log('This homepage is running at http://localhost:8080/index.html');
 });
